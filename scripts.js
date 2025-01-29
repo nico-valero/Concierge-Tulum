d48f9b0a3cb7453377f698f64ede6d1ce2d24b20
nico-valero <56361794+nico-valero@users.noreply.github.com>
2025-01-29 12:51:07 -0300

    Footer updated

--- scripts.js
+++ scripts.js
@@ -42,6 +42,6 @@ const firstCardWidth = carousel.querySelector('.carousel-card').offsetWidth;
 
 arrowBtns.forEach(btn => {
     btn.addEventListener('click', () => {
-        carousel.scrollLeft += btn.id === 'left' ? -firstCardWidth : firstCardWidth;
+        carousel.scrollLeft += btn.id === 'left' ? -firstCardWidth * 2 : firstCardWidth * 2;
     });
 });
