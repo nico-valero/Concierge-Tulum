eaa385fc3331d0b22aec71e9007814eb7847e479
nico-valero <56361794+nico-valero@users.noreply.github.com>
2025-06-14 03:32:37 -0300

    contact form

---
+++ src/contact.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstName = htmlspecialchars($_POST['name']);
+    $lastName = htmlspecialchars($_POST['last_name']);
+    $email = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "nico.valero97@gmail.com";
+    $subject = "New Contact Form Message";
+    $body = "Name: $firstName $lastName\n";
+    $body .= "Email: $email\n";
+    $body .= "Phone: $phone\n";
+    $body .= "Message:\n$message";
+
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Message failed to send.";
+    }
+}
+?>
